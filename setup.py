e6972da75b6867e9d52577bf7c0ceab9ecb0c694
Benji Huser-Berta <benj.huser@gmail.com>
2024-06-09 19:17:32 +0200

    Fix missing default for closed date column

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='montecarlocsv',
-    version='0.9.0',
+    version='0.9.1',
     packages=find_packages(),
     include_package_data=True,
     package_data={
