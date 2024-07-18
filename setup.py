c019d629ffe2e140d4bfff2646298434bf6c3cb9
Benji Huser-Berta <benj.huser@gmail.com>
2024-07-18 19:13:08 +0200

    version 0.9.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='montecarlocsv',
-    version='0.9.3',
+    version='0.9.4',
     packages=find_packages(),
     include_package_data=True,
     package_data={
