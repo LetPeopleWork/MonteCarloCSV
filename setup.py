66987d85e9e6ebf6d73156f1346e953012190e46
Benji Huser-Berta <benj.huser@gmail.com>
2024-09-27 13:31:17 +0200

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='montecarlocsv',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
     package_data={
