dac29aa2a57f05bf1512b5c033f9790b9015f82b
Gabor <gbittera@gmail.com>
2024-10-08 21:19:49 +0100

    bump to 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='montecarlocsv',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     package_data={
